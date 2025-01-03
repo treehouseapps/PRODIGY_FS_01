9bc91ffbbd25e824ac024359c1d64c0e3ee07748
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2025-01-03 20:12:26 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: './index.html', // Ensures correct entry point
+    },
+  },
+  server: {
+    historyApiFallback: true, // Enables fallback for SPA routes
+  },
+});
